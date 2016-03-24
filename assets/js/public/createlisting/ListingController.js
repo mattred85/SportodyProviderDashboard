0b5c8cab9df1419f3f55de32565e4f7168327c27
dejesus2010 <dejesus2010@gmail.com>
2016-03-24 15:46:30 -0400

    Add to extras fields

--- assets/js/public/createlisting/ListingController.js
+++ assets/js/public/createlisting/ListingController.js
@@ -1,10 +1,24 @@
 angular.module('ListingModule').controller('ListingController', function($scope, $http, $window){
 
+  // DESCRIPTION FORM
   $scope.descriptionForm = {};
 
   $scope.clickedDescriptionNext = function() {
     if ($scope.descriptionForm.listingName && $scope.descriptionForm.listingSummary) {
       $window.location.href = '/create_listing_extras?listingName=' + $scope.descriptionForm.listingName + '&listingSummary=' + $scope.descriptionForm.listingSummary;
     }
+  };
+
+  // EXTRAS FORM
+  $scope.extrasForm = {};
+  $scope.extrasForm.extras = [{id: 'extra1'}];
+  $scope.addExtra = function() {
+    var newExtraId = $scope.extrasForm.extras.length+1;
+    $scope.extrasForm.extras.push({'id':'extra'+newExtraId});
+    console.log($scope.extrasForm.extras[0]);
+  };
+  $scope.removeExtra = function(extra) {
+    var index = $scope.extrasForm.extras.indexOf(extra);
+    $scope.extrasForm.extras.splice(index, 1);
   };
 });
