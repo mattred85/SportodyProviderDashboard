c3e9617a63e8c47cd100bb2cb19134560cbcf5bf
dejesus2010 <dejesus2010@gmail.com>
2016-03-28 21:03:55 -0400

    Display extras in listing preview page

--- assets/js/public/listing_preview/PreviewController.js
+++ assets/js/public/listing_preview/PreviewController.js
@@ -4,7 +4,15 @@ angular.module('ListingModule').controller('PreviewController', function($scope,
   $scope.preview.calendar = $window.SAILS_LOCALS.listing.calendar;
   $scope.preview.title = $window.SAILS_LOCALS.listing.title;
   $scope.preview.summary = $window.SAILS_LOCALS.listing.summary;
-  $scope.preview.extras = $window.SAILS_LOCALS.extras;
+  $scope.preview.extras = JSON.parse($window.SAILS_LOCALS.listing.extras);
 
-  console.log($scope.preview);
+  $scope.addExtra = function() {
+    var newExtraId = $scope.preview.extras.length+1;
+    $scope.preview.extras.push({'id':'extra'+newExtraId});
+    console.log($scope.preview.extras[0]);
+  };
+  $scope.removeExtra = function(extra) {
+    var index = $scope.preview.extras.indexOf(extra);
+    $scope.preview.extras.splice(index, 1);
+  };
 });
